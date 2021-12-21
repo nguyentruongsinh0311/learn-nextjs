f00a63fc549dbe7fbdad14bee04f1c6e49a215d6
sinhnt <sinhnt.0311@gmail.com>
2021-12-21 16:27:22 +0700

    add prettier

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -9,5 +9,5 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'Sinh Nguyen Truong' })
+  res.status(200).json({ name: "Sinh NT" })
 }
